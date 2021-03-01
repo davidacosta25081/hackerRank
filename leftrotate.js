22649f3e2991b2e39af67f365f3e4084dd22cae8
David <davidacosta25081@yahoo.com>
2021-02-28 23:33:15 -0500

    left rotation of array

---
+++ leftrotate.js
@@ -0,0 +1,14 @@
+function rotleft(arr,rotations){
+  const rotatedArray = arr.concat();
+  for (i = 0 ; i < rotations ; i++) {
+    let frontElement = rotatedArray.shift();
+    rotatedArray.push(frontElement);
+  }
+  return rotatedArray;
+} 
+
+const n = 3;
+const sampleArray = [1,2,3,4,5,6];
+console.log(rotleft(sampleArray,3));
+
+
