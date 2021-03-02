fd422b6ea10f5a32fe79654466d548e3c6d403c4
David <davidacosta25081@yahoo.com>
2021-03-01 23:54:31 -0500

    valley's count

---
+++ countingvalleys.js
@@ -0,0 +1,18 @@
+function countingvalleys(steps, path) {
+  let numberOfValleys = 0;
+  let elevation = 0;
+
+
+  for (let i = 0 ; i < steps ; i++) {
+    if (path[i] == 'D') {
+      elevation--;
+    } 
+    else {
+      if (elevation == -1 ) {
+        numberOfValleys++;    
+      }
+      elevation++;
+    }
+}
+return numberOfValleys;
+}
