681546cb82d14667b81ece0110835d4143361bd5
David <davidacosta25081@yahoo.com>
2021-03-05 23:56:00 -0500

    completed fucntion

--- gradingStudents.js
+++ gradingStudents.js
@@ -1,13 +1,13 @@
 function gradingStudents(grades) {
-.
-
-
-
-
-
-
-
-
-
-    
+  return grades.map(grade => {
+  if (((grade + 1) %5)  == 0 && grade >=38) {  
+    return (grade + 1)  
+  }
+  else if (((grade+2)%5) == 0 && grade >= 38) {
+      return (grade+2)
+  }
+  else {
+    return grade;
+  }
+ })
 }
