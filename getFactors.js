6636e09a40507460a1f952f11f1f1090d6035fc2
David <davidacosta25081@yahoo.com>
2021-03-07 23:45:39 -0500

    pusing factors in

--- getFactors.js
+++ getFactors.js
@@ -1,9 +1,14 @@
 function getFactors(number) {
-
-
+  const factors = []
+  for (let i = 0 ; i <=number ; i++) {
+    if(number % i == 0){
+      factors.push(i);    
+    }
+  }
+  return factors;
+} 
 
 
 
 
     
-}
