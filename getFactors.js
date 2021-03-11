e95f29127c2e4f9e440a33b70e744e6f1c44dca1
David <davidacosta25081@yahoo.com>
2021-03-10 23:20:49 -0500

    getting common values function

--- getFactors.js
+++ getFactors.js
@@ -8,11 +8,33 @@ function getFactors(number) {
   return factors;
 } 
 
-Array.prototype.includesAll = function(arr1) {
-  return arr1.filter(el => this.includes(el)).length == arr1.length
-                           .
+  Array.prototype.includesAll = function(arr1) {
+    return arr1.filter(el => this.includes(el)).length == arr1.length
+}
 
-                          }
+  const getCommonValues = (arr) => {
+    const myObj = {};
+    arr.forEach(subArr => {
+      subArr.forEach(el => {
+        if (myObj.hasOwnProperty(el)){
+          myObj[el]++
+        }
+        else {
+          myObj[el] = 1;
+        }
+      })
+    })
+
+  Object.keys(myObj).forEach(key => {
+    if(myObj[key] != arr.length) {
+      delete myObj[key]
+    }
+  })
+  return Object.keys(myObj)
+  }
+
+  
+
 
 
     
