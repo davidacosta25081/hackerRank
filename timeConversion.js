e5e1a5bbe0bc2fb64f1a5ceaa52b2078c82b809f
David <davidacosta25081@yahoo.com>
2021-03-15 23:36:58 -0400

    else ifs on function

--- timeConversion.js
+++ timeConversion.js
@@ -2,8 +2,11 @@ function timeConversion(hour, type) {
   if (type == 'a' && hour == 12){
     return '00'    
   }
-
-
+  else if(type == 'a') {
+    if(hour<10){
+      return '0' + hour.toString()
+  }
+  
 
 
 
