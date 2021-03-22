fe5d525a3bea9fcb030ccf0b973dc9c1d0b3546d
David <davidacosta25081@yahoo.com>
2021-03-21 23:56:30 -0400

    maximumToys

---
+++ markAndToys.js
@@ -0,0 +1,23 @@
+/* 
+Mark and Jane are very happy after having their first child. 
+Their son loves toys, so Mark wants to buy some. 
+There are a number of different toys lying in front of him, 
+tagged with their prices. Mark has only a certain amount to spend, and he wants to maximize the number of toys he buys with this money. 
+Given a list of toy prices and an amount to spend, determine the maximum number of gifts he can buy.
+
+Example 
+
+prices = [1,2,3,4]
+k = 7 (Money he can spend)
+
+He could buy : [1,2,3] for 6 or [3,4] for 7.
+Te MAXIMUM is = 3 items.
+
+*/
+
+function maximumToys(prices,k) {
+
+
+
+    
+}
