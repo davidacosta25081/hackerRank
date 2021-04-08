20bdf7add5a6153deff0e2e8a63597ebc3427fa4
David <davidacosta25081@yahoo.com>
2021-04-07 23:56:29 -0400

    completing for loop and if statements

--- compareTriples.js
+++ compareTriples.js
@@ -2,9 +2,19 @@ function compareTriplets(a,b) {
 
   let alicePoints = 0 , bobPoints = 0;
   for (let i = 0; i<3; i++){
-
-    
+    if (a[i] > b[i]) {
+        alicepoints++;
+    }
+    else if (a[i]< b[i]){
+        bobPoints++;
+    }
   }
 
-    
+   return [alicePoints,bobPoints]   
 }
+
+
+let a = [3,100,200]
+let b = [1000,23,43]
+
+console.log(compareTriplets(a,b))
