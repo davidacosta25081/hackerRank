39a15c9b2c131940b18f50ef720aaaa13424614b
David <davidacosta25081@yahoo.com>
2021-04-13 23:53:36 -0400

    repeatedString function

---
+++ repeatedString.js
@@ -0,0 +1,16 @@
+function repeatedString(s,n){
+
+
+
+
+
+
+
+}
+
+/*
+Lilah has a string, s, of lowercase English letters that she repeated infinitely many times. 
+Given an integer, n , find and print the number of letter a's in the first n letters of Lilah's infinite string.
+For example, if the string s = 'abcac' and n = 10, the substring we consider is abcacabcac, the first 10 characters of her infinite string. 
+There are 4 occurrences of a in the substring.
+*/
